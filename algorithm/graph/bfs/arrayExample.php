e61ab48d5106bf8440984adece0e4767537d5636
Ziaur Rahman <zrshishir@clementinedatabd.com>
2020-05-31 18:15:19 +0600

    bfs practising

--- algorithm/graph/bfs/arrayExample.php
+++ algorithm/graph/bfs/arrayExample.php
@@ -8,4 +8,18 @@ for($i = 1; $i <= $vertexCount; $i++){
     $visited[$i] = 0;
 }
 
-print_r($visited);
+$graph[1][2] = $graph[2][1] = 1;
+$graph[1][5] = $graph[5][1] = 1;
+$graph[5][2] = $graph[2][5] = 1;
+$graph[5][4] = $graph[4][5] = 1;
+$graph[4][3] = $graph[3][4] = 1;
+$graph[3][2] = $graph[2][3] = 1;
+$graph[6][4] = $graph[4][6] = 1;
+
+$queue = new SplQueue;
+$queue->enqueue(1);
+$node = $queue->dequeue();
+
+foreach ($graph[$node] as $key => $value) {
+	echo "visited: $visited[$key] key: $key value: $value \n";
+}
