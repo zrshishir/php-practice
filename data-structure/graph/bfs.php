9d82046bd50b41a1ff1081d551253867aa3eae41
Ziaur Rahman <zrshishir@clementinedatabd.com>
2020-07-25 00:20:08 +0600

    starting implementing graph bfs

---
+++ data-structure/graph/bfs.php
@@ -0,0 +1,20 @@
+<?php
+
+$graph = [];
+$visited = [];
+$vertexCount = 6;
+
+for($i = 1; $i <= $vertexCount; $i++){
+    $graph[$i] = array_fill(1, $vertexCount, 0);
+    $visited[$i] = 0;
+}
+
+$graph[1][2] = $graph[2][1] = 1;
+$graph[1][5] = $graph[5][1] = 1;
+$graph[5][2] = $graph[2][5] = 1;
+$graph[5][4] = $graph[4][5] = 1;
+$graph[4][3] = $graph[3][4] = 1;
+$graph[3][2] = $graph[2][3] = 1;
+$graph[6][4] = $graph[4][6] = 1;
+
+var_dump($graph);
