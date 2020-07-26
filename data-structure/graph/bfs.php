8e5ce1734c173ac7c1b94dae72095573705dddf2
Ziaur Rahman <zrshishir@clementinedatabd.com>
2020-07-26 23:36:47 +0600

    implementing bgs using php7

--- data-structure/graph/bfs.php
+++ data-structure/graph/bfs.php
@@ -17,4 +17,33 @@ $graph[4][3] = $graph[3][4] = 1;
 $graph[3][2] = $graph[2][3] = 1;
 $graph[6][4] = $graph[4][6] = 1;
 
-var_dump($graph);
+// var_dump($graph[1]);
+// foreach($graph[1] as $key => $value){
+//     echo $key . '=>' . $value . "\t";
+// }
+// return true;
+function BFS(array &$graph, int $start, array $visited): SplQueue {
+    $queue = new SplQueue;
+    $path = new SplQueue;
+
+    $queue->enqueue($start);
+    $visited[$start] = 1;
+
+    while(! $queue->isEmpty()){
+        $node = $queue->dequeue();
+        $path->enqueue($node);
+        foreach($graph[$node] as $key => $value){
+            if(! $visited[$key] && $value == 1){
+                $visited[$key] = 1;
+                $queue->enqueue($key);
+            }
+        }
+    }
+
+    return $path;
+}
+$path = BFS($graph, 1, $visited);
+while(! $path->isEmpty()){
+    echo $path->dequeue() . "\t";
+}
+echo "\n";
