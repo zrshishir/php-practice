f3a5ee0e7c3125755de8924b0c79cfa194bbb2f4
Ziaur Rahman <zrshishir@clementinedatabd.com>
2020-07-29 23:59:44 +0600

    adding dfs and bfs

--- data-structure/graph/dfs.php
+++ data-structure/graph/dfs.php
@@ -1,11 +1,10 @@
-<?php
-
+<?php 
 $graph = [];
 $visited = [];
-$vertexCount = 6;
+$vertexCout = 6;
 
-for($i = 1; $i <= $vertexCount; $i++){
-    $graph[$i] = array_fill(1, $vertexCount, 0);
+for($i = 1; $i <= $vertexCout; $i++){
+    $graph[$i] = array_fill(1, $vertexCout, 0);
     $visited[$i] = 0;
 }
 
@@ -17,30 +16,32 @@ $graph[4][3] = $graph[3][4] = 1;
 $graph[3][2] = $graph[2][3] = 1;
 $graph[6][4] = $graph[4][6] = 1;
 
-// var_dump($graph);
 
-function BFS(array &$graph, int $start, array $visited): SplQueue {
-    $queue = new SplQueue;
+function DFS(array &$graph, int $start, array $visited): SplQueue{
+    $stack = new SplStack;
     $path = new SplQueue;
 
-    $queue->enqueue($start);
+    $stack->push($start);
     $visited[$start] = 1;
 
-    while(! $queue->isEmpty()){
-        $node = $queue->dequeue();
+    while(! $stack->isEmpty()){
+        $node = $stack->pop();
         $path->enqueue($node);
+
         foreach($graph[$node] as $key => $value){
             if(! $visited[$key] && $value == 1){
                 $visited[$key] = 1;
-                $queue->enqueue($key);
+                $stack->push($key);
             }
         }
     }
 
     return $path;
 }
-$path = BFS($graph, 1, $visited);
+
+$path = DFS($graph, 1, $visited);
+
 while(! $path->isEmpty()){
-    echo $path->dequeue() . "\t";
+    echo $path->dequeue(). "\t";
 }
 echo "\n";
