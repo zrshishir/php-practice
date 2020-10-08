2c093064c1b5e690e78bfb0f8f5ea44f7ef5ad66
Ziaur Rahman <zrshishir@clementinedatabd.com>
2020-10-08 22:16:58 +0600

    floyd warshall algorithm implemented

--- algorithm/shortest_path/floyd_worshall.php
+++ algorithm/shortest_path/floyd_worshall.php
@@ -1,5 +1,5 @@
 <?php 
-
+//floyd warshal algo is used to identify the shortest path between two nodes
 $totalVertices = 5;
 $graph = [];
 
@@ -19,17 +19,17 @@ $graph[1][3] = $graph[3][1] = 5;
 $graph[1][4] = $graph[4][1] = 10;
 $graph[3][4] = $graph[4][3] = 20;
 
-
 function floydWarshall(array $graph): array {
         $dist = [];
         $dist = $graph;
         $size = count($dist);
         for ($k = 0; $k < $size; $k++)
             for ($i = 0; $i < $size; $i++)
-                for ($j = 0; $j < $size; $j++)
+                for ($j = 0; $j < $size; $j++){
                     $dist[$i][$j] = min($dist[$i][$j],$dist[$i][$k] + $dist[$k][$j]);
+                }
         return $dist;
 }
 
 $distance = floydWarshall($graph);
-print_r($distance);
+echo "the distance between node 0 to 4: " . $distance[0][4]. "\n";
