6a8d58fabad300de50cd4cd11707c4a3f65b2845
Ziaur Rahman <zrshishir@clementinedatabd.com>
2020-10-17 14:29:03 +0600

    numeric literal separator

---
+++ php74-new-feature/numeric_literal_seprerator.php
@@ -0,0 +1,9 @@
+<?php 
+
+
+$float = 6.674_083e-11; // float
+$decimal = 299_792_458;   // decimal
+$hexadecimal = 0xCAFE_F00D;   // hexadecimal
+$binary = 0b0101_1111;   // binary
+
+print_r($float. "\n" . $decimal. "\n". $hexadecimal . "\n" . $binary);
