f4c149f57d05c05cd64910be531a76a562095166
Ziaur Rahman <zrshishir@gmail.com>
2020-12-16 00:21:07 +0600

    class construct

---
+++ design-pattern/part1/chapt1/TellAll.php
@@ -0,0 +1,12 @@
+<?php
+
+class TellAll{
+    private $userAgent;
+
+    public function __construct(){
+        // $this->userAgent = $_SERVER['HTTP_USER_AGENT'];
+        echo 'test';
+    }
+}
+
+$tallAll = new TellAll();
---
+++ oop/autoloading_classes/ex_01_autoload.php
@@ -0,0 +1,4 @@
+<?php
+spl_autoloan_register(funciton ($className){
+    include $className .'.php';
+});
