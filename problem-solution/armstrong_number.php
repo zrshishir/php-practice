28d3414577563a4fe9548527faf5f793cf882b2a
Ziaur Rahman <zrshishir@gmail.com>
2020-12-29 15:26:33 +0600

    some edition

--- problem-solution/armstrong_number.php
+++ problem-solution/armstrong_number.php
@@ -1,22 +1,22 @@
 <?php
+
 $startTime = microtime(true);
 $startMemory = memory_get_usage();
 $count = 0;
 
-
 function armostrongNumber(int $n): void{
-    $total = 0;
-    $x = $n;
-    while($n != 0){
-        $remainder = $n % 10;
-        $total = $total + $remainder * $remainder * $remainder;
-        $n = $n / 10;
-    }
-    if($x == $total){
-        echo "$x is an armostrong no.\n";
-    }else{
-        echo "$x is not an armostrong no. \n";
-    }
+	$total = 0;
+	$x = $n;
+	while ($n != 0) {
+		$remainder = $n % 10;
+		$total = $total + $remainder * $remainder * $remainder;
+		$n = $n / 10;
+	}
+	if ($x == $total) {
+		echo "$x is an armostrong no.\n";
+	} else {
+		echo "$x is not an armostrong no. \n";
+	}
 }
 
 //calling function and giving output here
@@ -25,5 +25,5 @@ armostrongNumber($number);
 
 $endTime = microtime(true);
 $endMemory = memory_get_usage();
-echo "execution time: " . ($endTime - $startTime) . "\n";
-echo "memroy used: " . ($endMemory - $startMemory) / (1024 * 1024) . "\n";
+echo "<br/>execution time: " . ($endTime - $startTime) . "\n";
+echo "<br/>memroy used: " . ($endMemory - $startMemory) / (1024 * 1024) . "\n";
