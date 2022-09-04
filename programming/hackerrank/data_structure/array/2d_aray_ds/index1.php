1b1424c76dfd7d8a74d2d134ab9649db8d392fc6
xs-ziaur <ziaur@xpeedstudio.com>
2022-09-04 18:25:38 +0600

    problem solution for 2d array ds solved and accepted

--- programming/hackerrank/data_structure/array/2d_aray_ds/index1.php
+++ programming/hackerrank/data_structure/array/2d_aray_ds/index1.php
@@ -32,12 +32,20 @@ function hourGlassSum($arr) {
 
 $fptr = fopen("output.txt", "w");
 
-$file = fopen("input.txt", "r");
+// $file = fopen("input1.txt", "r");
 
-$arr = array();
-while (!feof($file)) {
-    $arr[] = explode( ' ', fgets( $file ) );
-}
+$arr = [
+    [-1, -1, 0, -9, -2, -2],
+[-2, -1, -6, -8, -2, -5],
+[-1, -1, -1, -2, -3, -4],
+[-1, -9, -2, -4, -4, -5],
+[-7, -3, -3, -2, -9, -9],
+[-1, -3, -1, -2, -4, -5]
+];
+
+// while (!feof($file)) {
+//     $arr[] = explode( ' ', fgets( $file ) );
+// }
 
 $result = hourGlassSum($arr);
 
