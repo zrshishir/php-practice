158912273bdfa3af6fe9aff2cb2f907f82314371
xs-ziaur <ziaur@xpeedstudio.com>
2022-09-12 18:03:47 +0600

    wrap up the solution

--- programming/hackerrank/basic/between_two_set/index.php
+++ programming/hackerrank/basic/between_two_set/index.php
@@ -9,7 +9,35 @@
  */
 function getTotalX($n, $arr, $m, $brr) {
     // Write your code here
-   var_dump($arr[ $n - 1 ], $brr[ $m - $m ]);
+    $counter = 0;
+    
+    for ($i = $arr[ $n - 1 ] ; $i <= $brr[ $m - 1 ] ; $i++) { 
+        $tag = false;
+
+        foreach( $arr as $value ) {
+            if ( $i % $value != 0 ) {
+                $tag = false;
+                break;
+            } else {
+                $tag = true;
+            }
+        }
+
+        foreach( $brr as $vl ) {
+            if ( $vl % $i != 0) {
+                $tag = false;
+                break;
+            } else {
+                $tag = true;
+            }
+        }
+
+        if ( $tag ) {
+            $counter++;
+        }
+    }
+   
+    return $counter;
 }
 
 $fptr = fopen(("output.txt"), "w");
@@ -25,7 +53,7 @@ $barrTemp = rtrim( fgets( STDIN ) );
 $brr = array_map('intval', preg_split('/ /', $barrTemp, -1, PREG_SPLIT_NO_EMPTY));
 
 $result = getTotalX($n, $arr, $m, $brr);
-
+var_dump($result);
 fwrite($fptr, print_r( $result, true ) );
 
 fclose($fptr);
