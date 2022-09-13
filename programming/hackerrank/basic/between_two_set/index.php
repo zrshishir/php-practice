cea1f1564cfd42a58942dcac41591301c9122b7a
xs-ziaur <ziaur@xpeedstudio.com>
2022-09-13 11:05:21 +0600

    between two set problem solved

--- programming/hackerrank/basic/between_two_set/index.php
+++ programming/hackerrank/basic/between_two_set/index.php
@@ -11,33 +11,41 @@ function getTotalX($n, $arr, $m, $brr) {
     // Write your code here
     $counter = 0;
     
-    for ($i = $arr[ $n - 1 ] ; $i <= $brr[ $m - 1 ] ; $i++) { 
+    for ($i = $arr[ 0 ] ; $i <= $brr[ $m - 1 ] ; $i++) { 
         $tag = false;
 
-        foreach( $arr as $value ) {
-            if ( $i % $value != 0 ) {
-                $tag = false;
-                break;
-            } else {
-                $tag = true;
-            }
-        }
-
-        foreach( $brr as $vl ) {
-            if ( $vl % $i != 0) {
-                $tag = false;
-                break;
-            } else {
-                $tag = true;
-            }
-        }
+        $tag = itemDivided($i, $arr);
 
         if ( $tag ) {
-            $counter++;
+            $tag = itemDivisor( $i, $brr);
+            
+            if ( $tag ) {
+                $counter++;
+            }
         }
     }
    
     return $counter;
+}
+
+function itemDivided($item, $arr) {
+    foreach ($arr as $value) {
+        if ( $item % $value != 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function itemDivisor($item, $arr) {
+    foreach ($arr as $value) {
+        if ( $value % $item != 0) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 $fptr = fopen(("output.txt"), "w");
@@ -53,7 +61,7 @@ $barrTemp = rtrim( fgets( STDIN ) );
 $brr = array_map('intval', preg_split('/ /', $barrTemp, -1, PREG_SPLIT_NO_EMPTY));
 
 $result = getTotalX($n, $arr, $m, $brr);
-var_dump($result);
+
 fwrite($fptr, print_r( $result, true ) );
 
 fclose($fptr);
