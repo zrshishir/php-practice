de149e9696e8f1bf0cd0e49f9a06cd7dbb364bca
zrshishir <zrshishir@gmail.com>
2022-10-11 11:24:39 +0600

    logical implementation

--- programming/hackerrank/data_structure/string/super_reduced_string/index.php
+++ programming/hackerrank/data_structure/string/super_reduced_string/index.php
@@ -2,7 +2,15 @@
 
 
 function superReducedString( $s ) {
-    print(implode( "", array_unique( str_split($s) ) ));
+    $strings = str_split($s);
+    for($i = 0; $i < strlen($s); $i++) {
+        
+    }
+    print_r($strings);
+    // for($i = 0; $i < strlen($s); $i++) {
+
+    // }
+    // print(implode( "", array_unique( str_split($s) ) ));
 }
 
 $fptr = fopen(("output.txt"), "w");
