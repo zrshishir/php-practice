96947eaea9fee6599f2c3dd651ec53f88a50f07c
zrshishir <zrshishir@gmail.com>
2022-10-11 11:27:38 +0600

    simple edit

--- programming/hackerrank/data_structure/string/super_reduced_string/index.php
+++ programming/hackerrank/data_structure/string/super_reduced_string/index.php
@@ -4,13 +4,9 @@
 function superReducedString( $s ) {
     $strings = str_split($s);
     for($i = 0; $i < strlen($s); $i++) {
-        
+
     }
     print_r($strings);
-    // for($i = 0; $i < strlen($s); $i++) {
-
-    // }
-    // print(implode( "", array_unique( str_split($s) ) ));
 }
 
 $fptr = fopen(("output.txt"), "w");
