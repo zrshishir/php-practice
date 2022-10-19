ac277e0958b181ee7b0b7d1b4e97e4eb4bd5d1aa
zrshishir <zrshishir@gmail.com>
2022-10-19 09:31:32 +0600

    algorithm changed

--- programming/hackerrank/data_structure/string/super_reduced_string/index.php
+++ programming/hackerrank/data_structure/string/super_reduced_string/index.php
@@ -2,16 +2,31 @@
 
 
 function superReducedString( $s ) {
-    $strings = str_split($s);
 
-    for($i = 0; $i < strlen($s); $i++) {
-        if($strings[$i] == $strings[$i+1]) {
+    $strings = str_split($s);
+    $n = strlen($s);
+    $output = "";
+
+    for($i = 0; $i < $n; $i++) {
+        if( isset($strings[$i + 1]) && $strings[$i] == $strings[$i + 1]) {
             $strings[$i] = '';
-            $strings[$i+1] = '';
+            $strings[$i + 1] = '';
+
+            if (($i > 0 && isset($strings[$i + 2])) && $strings[$i - 1] == $strings[$i + 2]) {
+                print_r($output);
+                $strings[$i - 1] = '';
+                $strings[$i + 2] = '';
+                $output = str_replace($strings[$i - 1], '', $output);
+            }
         }
+
+        if( !empty($strings[$i]) ){
+            $output .= $strings[$i];
+        }
+        
     }
     
-    return array_values(array_unique($strings));
+    return ($output ?: "Empty String");
 }
 
 $fptr = fopen(("output.txt"), "w");
@@ -19,8 +34,7 @@ $fptr = fopen(("output.txt"), "w");
 $s = rtrim(fgets(STDIN), "\r\n");
 
 $result = superReducedString($s);
-var_dump($result);
-fwrite($fptr, $result . "\n");
 
+fwrite($fptr, print_r($result) . "\n");
 
 fclose($fptr);
